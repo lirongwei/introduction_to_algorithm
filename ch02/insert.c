72fe8eab4e11b915f491f9ef09c9b2c6dd5a9160
lirongwei <lirongwei@xiaomi.com>
2016-04-17 11:36:05 +0800

    add insert sort
    Signed-off-by: lirongwei <lirongwei@xiaomi.com>

---
+++ ch02/insert.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+void insert(int a[], int N) 
+{
+    for (int i = 1; i < N; i++)
+    {
+        for (int j = i - 1; j >= 0; j--)
+        {
+            if (a[j + 1] > a[j]) 
+            {
+                a[j + 1] = a[j + 1] + a[j];
+                a[j] = a[j + 1] - a[j];
+                a[j + 1] = a[j + 1] - a[j];
+            }
+        }
+    }
+}
+
+int main() 
+{
+    int a[10] = {3, 4, 5, 2, 9, 1, 8, 7, 6, 10};
+
+    insert(a, 10);
+    printf("hello\n");
+
+    for (int i = 0 ; i < 10; i++) 
+    {
+        printf("%d ", a[i]);
+    }
+
+    return 0;
+}
+
